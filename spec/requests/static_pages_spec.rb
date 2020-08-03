49d5e3fc99888fb833057f99b0594d78bf101525
YoooTooo <son.toyo.mcmxc@gmail.com>
2020-08-03 19:00:39 +0900

    wrote some system spec test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Access to static_pages', type: :request do
   context 'GET #home' do
     before { get root_path }
     it 'responds successfully' do
-      expect(response).to have_http_status 200
+      expect(response).to have_http_status(:success)
     end
     it "has title 'LinGoChan'" do
       expect(response.body).to include 'LinGoChan'
@@ -15,7 +15,7 @@ RSpec.describe 'Access to static_pages', type: :request do
   context 'GET #about' do
     before { get about_path }
     it 'responds successfully' do
-      expect(response).to have_http_status 200
+      expect(response).to have_http_status(:success)
     end
     it "has title 'LinGoChan / LinGoChanって何'" do
       expect(response.body).to include 'LinGoChan / LinGoChanって何'
