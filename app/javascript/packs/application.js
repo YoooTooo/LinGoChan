a926dbaaf618554ea29542c6df1263c1e3609878
YoooTooo <son.toyo.mcmxc@gmail.com>
2020-09-19 16:17:43 +0900

    modified some files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from '@rails/ujs';
-Rails.start();
-
 import 'bootstrap';
 import '../stylesheets/application.scss';
 import './bootstrap_custom.js'
 
+const images = require.context('../images/', true)
 
+require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery')
+require("jquery")
 require("bootstrap");
 require('load_animation.js')
 
