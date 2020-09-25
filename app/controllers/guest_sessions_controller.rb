d24f1448b96b3e6fdbdcdb656168dc6c873ec403
YoooTooo <son.toyo.mcmxc@gmail.com>
2020-09-25 22:31:39 +0900

    modified guest controller#create to work on (take 4)

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -1,14 +1,12 @@
 class GuestSessionsController < ApplicationController
 
   def create
-    if user = User.find(999)
-      log_in(user)
-      redirect_to user_path(user)
-    else
-      user = User.create(id:999 ,name: 'りんごちゃん', email: 'easy_login@example.com')
-      log_in_as_guset(user) #application_controller.rbに記述してる。
-      flash[:success] = 'ゲストユーザーでログインしました'
-      redirect_to user_path(user)
-    end
+     user = User.find_or_create_by(email: 'guest@example.com')
+     user.name = "りんごちゃん"
+     user.password = "lingochan"
+     user.save if !user.id #idがない場合、DBに保存してIDを生成
+     log_in_as_guset(user)
+     redirect_to user_path(user)
+     flash[:success] = 'ゲストユーザーでログインしました'
   end
 end
