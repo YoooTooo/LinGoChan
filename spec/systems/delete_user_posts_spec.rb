3aa7368585a4f088c45a59ae4332ee09988ab37c
YoooTooo <son.toyo.mcmxc@gmail.com>
2020-10-12 16:11:52 +0900

    reformed feedback_post_edit

--- spec/systems/delete_user_posts_spec.rb
+++ spec/systems/delete_user_posts_spec.rb
@@ -5,12 +5,13 @@ RSpec.describe "administrator's move", type: :system do
   let(:administrator) { FactoryBot.create(:administrator) }
   let(:subject_post) { FactoryBot.create(:subject_post) }
 
-  user1 = User.create(id:1, name: "user1", email:"user1@example.com", password:"user1passw")
-  subject_post1 = SubjectPost.create(id:1, img: "xxxxxxxxxxxxx.png")
-  reply_post1 = ReplyPost.create(id:1, user_id:1, subject_post_id:1, content: "りんごちゃん")
-  feedback_post1 = FeedbackPost.create(id:1, content: "みかんちゃんですね")
+  before :each do
+    user1 = User.create(id:1, name: "user1", email:"user1@example.com", password:"user1passw")
+    @subject_post = SubjectPost.create(id:1, user_id:1,  img: "xxxxxxxxxxxxx.png")
+    @subject_post1 = SubjectPost.create(id:2, user_id:1,  img: "xxxxxxxxxxxxy.png")
+    @reply_post = ReplyPost.create(id:1, user_id:1, subject_post_id:1, content: "りんごちゃん")
+    @feedback_post = FeedbackPost.create(id:1, content: "みかんちゃんですね")
 
-  before do
     log_in(administrator)
   end
 
@@ -29,21 +30,22 @@ RSpec.describe "administrator's move", type: :system do
     expect(page).to have_button('削除')
     expect{ click_on '削除' }.to change(User, :count).by(-1)
     end
-
+  end
 
     #==========画像投稿(subject_post)の削除==========
     context "delete subject_post" do
       before "visit subject_post show" do
-        visit subject_post_path(subject_post)
-        expect(current_path).to eq subject_post_path(subject_post)
+        visit subject_post_path(@subject_post.id)
+        expect(current_path).to eq subject_post_path(@subject_post.id)
       end
-    end
+
 
     #削除を押下したらsubject_postが消える。
     it 'should delete a subject_post' do
       expect(page).to have_button('削除')
       expect{ click_on '削除' }.to change(SubjectPost, :count).by(0)
     end
+  end
 
     #==========ひとこと(reply_post)の削除==========
     context "delete reply_post" do
@@ -51,13 +53,13 @@ RSpec.describe "administrator's move", type: :system do
         visit reply_post_path(reply_post1)
         expect(current_path).to eq subject_post_path(reply_post1)
       end
-    end
 
     #削除を押下したらreply_postが消える。
     it 'should delete a reply_post' do
       expect(page).to have_button('削除')
       expect{ click_on '削除' }.to change(ReplyPost, :count).by(0)
     end
+  end
 
     #==========フィードバック(feedback_post)の削除==========
     context "delete reply_post" do
@@ -65,7 +67,6 @@ RSpec.describe "administrator's move", type: :system do
         visit feedback_post_path(feedback_post1)
         expect(current_path).to eq subject_post_path(feedback_post1)
       end
-    end
 
     #削除を押下したらfeedback_postが消える。
     it 'should delete a feedback_post' do
