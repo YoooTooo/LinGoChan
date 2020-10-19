c0660b9dc74c3f8c0eb5fce60b79cb9bfe36d857
YoooTooo <son.toyo.mcmxc@gmail.com>
2020-10-19 16:33:27 +0900

    modified post_spec

---
+++ spec/systems/posts_spec.rb
@@ -0,0 +1,19 @@
+equire 'pry'
+require 'rails_helper'
+
+
+RSpec.describe 'signup', type: :system do
+  let(:user) { FactoryBot.create(:user) }
+
+  before do
+    log_in(user)
+    expect(current_path).to eq "/users"
+  end
+
+  describe 'POST subject_post' do
+    visit root_path
+    expect(current_path).to eq "/"
+    attach_file "Upload file", "/app/assets/images/LinGoChan.png"
+    click_on("commit")
+  end
+end
